0427aab561661ed6328dd7f115ddbe53c85b9658
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-01-27 14:50:26 +0530

    Add files via upload

---
+++ Web Development/Javascript/01 basics.js
@@ -0,0 +1,22 @@
+2+3
+5
+alert("hello");
+undefined
+typeof(123);
+"number"
+typeof(true)
+"boolean"
+typeof("SHivendra")
+"string"
+prompt("Whai is your name")
+"shivendra "
+var name = "shivendra";
+undefined
+alert(name)
+undefined
+var yourname = prompt("What is your name");
+var yourname = prompt("What is your name");
+var yourname = prompt("What is your name");
+undefined
+undefined
+undefined
