ade38379d8e5ba763067989653621118f3252d0c
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-07-01 02:06:49 +0530

    Update 01 basics.js

--- Web Development/Javascript/01 basics.js
+++ Web Development/Javascript/01 basics.js
@@ -20,3 +20,46 @@ var yourname = prompt("What is your name");
 undefined
 undefined
 undefined
+
+function foo() {
+let a = b = 0;
+a++;
+return a;
+}
+undefined
+foo();
+typeof a; // => ???
+typeof b; // => ???
+"number"
+foo();
+
+typeof b; // => ???
+"number"
+typeof a;
+"undefined"
+let x = 1;
+{
+let x = 2;
+}
+console.log(x);
+VM208:5 1
+undefined
+let x = 1;
+{
+let x = 2;
+}
+console.log(x);
+VM213:5 1
+undefined
+let x0 = 0;
+(function autorun1(){
+let x1 = 1;
+(function autorun2(){
+let x2 = 2;
+(function autorun3(){
+let x3 = 3;
+console.log(x0 + " " + x1 + " " + x2 + " " + x3);
+})();
+})();
+})();
+VM219:8 0 1 2 3
