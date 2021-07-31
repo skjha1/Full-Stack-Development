c68029aa3c5339f2ff0361d1da1268aa18c91255
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-07-31 11:31:18 +0530

    Create 02 Read your name and print it.js

---
+++ Web Development/Javascript/TP/02 Read your name and print it.js
@@ -0,0 +1,6 @@
+// Here we installed the package readline sync
+var readlinesync = require('readline-sync');
+var username = readlinesync.question('May I know your name please ? ');
+console.log(username);
+
+// here what we did is just taken input from user and printed it
